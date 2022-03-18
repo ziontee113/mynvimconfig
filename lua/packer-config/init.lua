9f7b9b67cfe13439e75ebef6c6635e626df2ec07
Dev Masta <new@mail.com>
2022-03-18 15:46:46 +0700

    installed cmp plugins, config later

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -34,4 +34,16 @@ return require('packer').startup(function()
   use 'folke/tokyonight.nvim'
 
 
+  -- NVIM CMP Autocomplete
+  use 'neovim/nvim-lspconfig'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+
+  -- LuaSnip
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+
 end)
