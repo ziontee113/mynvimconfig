4fa734be188d67b8d47577d0ec5aaa47d8a83414
Dev Masta <new@mail.com>
2022-03-18 13:18:19 +0700

    added packer autocmd from chris@machine

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -1,3 +1,17 @@
+-- Autocommand that reloads neovim whenever you save the plugins.lua file (PackerSync on save)
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost ~/.config/nvim/lua/packer-config/init.lua source <afile> | PackerSync
+  augroup end
+]]
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Install your plugins here
 return require('packer').startup(function()
 
 	-- Packer can manage itself
