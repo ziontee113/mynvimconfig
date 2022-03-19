97e88359920bcf920317f5949f2daeadeb71b091
Dev Masta <new@mail.com>
2022-03-19 11:37:15 +0700

    toggle term file

--- init.lua
+++ init.lua
@@ -11,17 +11,5 @@ require("user.lualine")
 require("user.nvim-tree")
 require("user.mappings")
 
--- Toggle Term --
-require("toggleterm").setup({
-	size = 12,
-	open_mapping = [[<c-\>]],
-	shade_filetypes = {},
-	shade_terminals = true,
-	shading_factor = "1",
-	start_in_insert = true,
-	persist_size = true,
-	direction = "horizontal",
-})
-
 -- Comment.nvim --
 require("Comment").setup()
