574410b502a7a65844ca1d8ee41f418b4016ed98
Dev Masta <new@mail.com>
2022-03-19 10:24:07 +0700

    null-ls setup

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -1,13 +1,16 @@
 local null_ls = require("null-ls")
 
 local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
 	sources = {
 		formatting.prettierd,
-		formatting.eslint_d,
+		-- formatting.eslint_d,
 		formatting.stylua,
 		formatting.clang_format,
+
+		-- diagnostics.eslint_d,
 	},
 	on_attach = function(client) --> Format on Save
 		if client.resolved_capabilities.document_formatting then
