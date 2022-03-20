80abca4850eecf75d765822b44adbff1ec6d4b19
Dev Masta <new@mail.com>
2022-03-20 10:29:04 +0700

    we got it boys

--- lua/snippets/all.lua
+++ lua/snippets/all.lua
@@ -10,6 +10,7 @@ local snippets = {
 	s("req", fmt("require {} = require('{}')", { i(1, "default"), rep(1) })),
 	s("oddeye", { t("some text") }),
 	s("mamba", { t("some text") }),
+	s("black", { t("black "), i(1, "mamba") }),
 }
 
 return snippets
--- lua/user/luasnip-config/init.lua
+++ lua/user/luasnip-config/init.lua
@@ -5,6 +5,36 @@ ls.config.set_config({
 	enable_autosnippets = true,
 })
 
+-- Kep Mapping --
+
+-- <c-k> is my expansion key
+-- this will expand the current item or jump to the next item within the snippet.
+vim.keymap.set({ "i", "s" }, "<c-k>", function()
+	if ls.expand_or_jumpable() then
+		ls.expand_or_jump()
+	end
+end, { silent = true })
+
+-- <c-j> is my jump backwards key.
+-- this always moves to the previous item within the snippet
+vim.keymap.set({ "i", "s" }, "<c-j>", function()
+	if ls.jumpable(-1) then
+		ls.jump(-1)
+	end
+end, { silent = true })
+
+-- <c-l> is selecting within a list of options.
+-- This is useful for choice nodes (introduced in the forthcoming episode 2)
+vim.keymap.set("i", "<c-l>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end)
+
+vim.keymap.set("i", "<c-u>", require("luasnip.extras.select_choice"))
+
+-- Auto Reload --
+
 function _G.snippets_clear()
 	for m, _ in pairs(ls.snippets) do --> clear all snippets
 		package.loaded["snippets." .. m] = nil
