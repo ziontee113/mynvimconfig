d8751eaa1f61addf12d8d370d0d78320c46b4e38
Dev Masta <new@mail.com>
2022-03-20 07:52:47 +0700

    working on luasnip setup

---
+++ lua/user/luasnip/init.lua
@@ -0,0 +1,31 @@
+local luasnip = require("luasnip")
+
+function _G.snippets_clear()
+	for m, _ in pairs(luasnip.snippets) do
+		package.loaded["snippets." .. m] = nil
+	end
+	luasnip.snippets = setmetatable({}, {
+		__index = function(t, k)
+			local ok, m = pcall(require, "snippets." .. k)
+			if not ok and not string.match(m, "^module.*not found:") then
+				error(m)
+			end
+			t[k] = ok and m or {}
+
+			-- optionally load snippets from vscode- or snipmate-library:
+			--
+			-- require("luasnip.loaders.from_vscode").load({include={k}})
+			-- require("luasnip.loaders.from_snipmate").load({include={k}})
+			return t[k]
+		end,
+	})
+end
+
+_G.snippets_clear()
+
+vim.cmd([[
+augroup snippets_clear
+au!
+au BufWritePost ~/.config/nvim/lua/snippets/*.lua lua _G.snippets_clear()
+augroup END
+]])
