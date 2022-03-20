4f41fc0c40d1700db55e31fd55787cbdad57beb9
Dev Masta <new@mail.com>
2022-03-20 11:08:29 +0700

    I'm a savage

--- lua/snippets/all.lua
+++ lua/snippets/all.lua
@@ -6,12 +6,18 @@ local t = ls.t
 local fmt = require("luasnip.extras.fmt").fmt
 local rep = require("luasnip.extras").rep
 
+local function getSavage()
+	package.loaded["snippets.all.savage"] = nil
+	local tempVar = require("snippets.all.savage")
+	return tempVar
+end
+
 local snippets = {
 	s("req", fmt("require {} = require('{}')", { i(1, "default"), rep(1) })),
 	s("oddeye", { t("some text") }),
 	s("mamba", { t("some text") }),
 	s("black", { t("black "), i(1, "mamba") }),
-	s("test", { t("this is a test text node") }),
+	s("test", { t(getSavage()) }),
 }
 
 return snippets
