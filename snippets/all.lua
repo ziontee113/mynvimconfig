3152e74a1b829471f9dc958eb72e01cf456f7174
Dev Masta <new@mail.com>
2022-03-29 04:18:26 +0700

    testing some snippets

--- snippets/all.lua
+++ snippets/all.lua
@@ -1,6 +1,16 @@
+-- Imports
 local ls = require("luasnip")
 local s = ls.s
-local f = ls.f
+local i = ls.i
+local t = ls.t
+
+local d = ls.dynamic_node
+local c = ls.choice_node
+local f = ls.function_node
+local sn = ls.snippet_node
+
+local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
 
 return {
 	ls.parser.parse_snippet("ctrig", "also loaded!!"),
@@ -9,6 +19,10 @@ return {
 		f(function(_, snip)
 			return snip.env.SELECT_RAW
 		end, {}),
+	}),
+	s("insertnode", {
+		i(1, "shiro"),
+		i(2, "venus"),
 	}),
 }, {
 	ls.parser.parse_snippet("autotrig", "autotriggered, if enabled"),
--- snippets/all/trust.lua
+++ snippets/all/trust.lua
@@ -1,9 +1,9 @@
-local ls = require("luasnip")
-
-return {
-	ls.parser.parse_snippet("toofar", "boom"),
-	ls.parser.parse_snippet("expect", "pressure"),
-}, {
-
-	ls.parser.parse_snippet("hello", "shiro"),
-}
+-- local ls = require("luasnip")
+--
+-- return {
+-- 	ls.parser.parse_snippet("toofar", "boom"),
+-- 	ls.parser.parse_snippet("expect", "pressure"),
+-- }, {
+--
+-- 	ls.parser.parse_snippet("hello", "HELLO!"),
+-- }
