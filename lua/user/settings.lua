3a6c732ae1be2dcfb6ee3b8706778c678c7365af
Dev Masta <banhhangemra001@gmail.com>
2022-04-01 03:29:55 +0700

    text yank post

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -65,3 +65,11 @@ vim.cmd([[:hi Folded guibg=NONE]])
 
 -- Codi Text Color
 -- vim.cmd([[hi CodiVirtualText guifg=skyblue]])
+
+-- Text Yank Post
+vim.cmd([[
+augroup highlight_yank
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=200}
+augroup END
+]])
