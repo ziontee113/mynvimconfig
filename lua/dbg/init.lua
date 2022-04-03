9029ac84de6df827eb49754b61783ac191ad7f2c
Dev Masta <ziontee113@gmail.com>
2022-04-03 08:00:28 +0700

    still trying more

--- lua/dbg/init.lua
+++ lua/dbg/init.lua
@@ -1,54 +1,18 @@
 require("telescope").load_extension("dap")
 require("dbg.python")
 
+-- nvim-dap-virtual-text
+require("nvim-dap-virtual-text").setup()
+
+-- nvim-dap-ui
+require("dapui").setup()
+
 -- Key Maps
--- vim.cmd([[
--- " Create a submode to handle debugging
---
--- " The submode is entered whith <Leader>j and exited with <Leader>
--- call submode#enter_with('DebugMode', 'n', '', '<Leader>j', ':echo "debug mode"<CR>')
--- call submode#leave_with('DebugMode', 'n', '', '<Leader>')
---
--- "
--- call submode#map('DebugMode', 'n', '', 'h', '<cmd>lua require"dap".step_out()<CR>')
--- call submode#map('DebugMode', 'n', '', 'j', '<cmd>lua require"dap".step_over()<CR>')
--- call submode#map('DebugMode', 'n', '', 'k', '<cmd>lua require"dap".step_into()<CR>')
--- call submode#map('DebugMode', 'n', '', 'l', '<cmd>lua require"dap".continue()<CR>')
--- " call submode#map('DebugMode', 'n', '', 'o', '<cmd>lua print("Hello World")<CR>')
---
--- call submode#map('DebugMode', 'n', '', 'b', '<cmd>lua require"dap".toggle_breakpoint()<CR>')
---
--- call submode#map("DebugMode", "n", '', 's', '<cmd>lua require"dap.ui.variables".scopes()<CR>')
--- call submode#map("DebugMode", "n", '', 'H', '<cmd>lua require"dap.ui.variables".hover()<CR>')
--- call submode#map("DebugMode", "n", '', 'vh', '<cmd>lua require"dap.ui.variables".visual_hover()<CR>')
---
--- call submode#map("DebugMode", "n", '', 'ro', '<cmd>lua require"dap".repl.open()<CR>')
--- call submode#map("DebugMode", "n", '', 'rl', '<cmd>lua require"dap".repl.run_last()<CR>')
---
--- "
---
--- call submode#map("DebugMode", "n", '', 'wh', '<cmd>lua require"dap.ui.widgets".hover()<CR>')
--- call submode#map("DebugMode", "n", '', 'wf', '<cmd>lua local widgets=require"dap.ui.widgets";widgets.centered_float(widgets.scopes)<CR>')
---
--- call submode#map("DebugMode", "n", '', 'sb', '<cmd>lua require"dap".set_breakpoint(vim.fn.input("Breakpoint condition: "))<CR>')
--- call submode#map("DebugMode", "n", '', 'sm', '<cmd>lua require"dap".set_breakpoint(nil, nil, vim.fn.input("Log point message: "))<CR>')
---
--- " telescope-dap
---
--- call submode#map("DebugMode", "n", '', 'dc', '<cmd>lua require"telescope".extensions.dap.commands{}<CR>')
--- call submode#map("DebugMode", "n", '', 'cf', '<cmd>lua require"telescope".extensions.dap.configurations{}<CR>')
--- call submode#map("DebugMode", "n", '', 'lb', '<cmd>lua require"telescope".extensions.dap.list_breakpoints{}<CR>')
--- call submode#map("DebugMode", "n", '', 'dv', '<cmd>lua require"telescope".extensions.dap.variables{}<CR>')
--- call submode#map("DebugMode", "n", '', 'df', '<cmd>lua require"telescope".extensions.dap.frames{}<CR>')
---
--- "
---
--- let g:submode_keep_leaving_key = 0
--- let g:submode_timeout = 0
--- ]])
 
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
+
+keymap("n", "<leader>dui", '<cmd>lua require"dapui".toggle()<CR>', opts)
 
 keymap("n", "<leader>dct", '<cmd>lua require"dap".continue()<CR>', opts)
 keymap("n", "<leader>dsv", '<cmd>lua require"dap".step_over()<CR>', opts)
---
+++ test.js
@@ -0,0 +1,4 @@
+let x = 10;
+let y = 100;
+
+console.log(x + y);
