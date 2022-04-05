bddde5783725ae3989ec47b9f754a155a4b46e7c
Dev Masta <ziontee113@gmail.com>
2022-04-06 03:34:09 +0700

    small wiki change

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
+// write a recursive fucntion countDown(num) that prints the numbers from num to 0.
+function countDown(num) {
+  if (num <= 0) {
+    return "Done!";
+  } else {
+    console.log(num);
+    countDown(num - 1);
+  }
+}
 
+countDown(4);
