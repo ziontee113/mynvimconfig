056c0e8f825fb23a289c3fa0f3a037893e9a0644
Dev Masta <ziontee113@gmail.com>
2022-04-11 07:37:09 +0700

    closer and closer now

--- lua/myPlugs/init.lua
+++ lua/myPlugs/init.lua
@@ -22,10 +22,13 @@ local function get_master_node()
 		error("No Treesitter parser found")
 	end
 
+	local root = ts_utils.get_root_for_node(node)
+
 	local start_row = node:start()
 	local parent = node:parent()
 
-	while parent ~= nil and parent:start() == start_row do
+	while parent ~= nil and parent ~= root and parent:start() == start_row do
+		-- print(node:type())
 		node = parent
 		parent = node:parent()
 	end
@@ -42,6 +45,16 @@ M.select = function()
 	ts_utils.update_selection(bufnr, node)
 end
 
+M.move = function()
+	local node = get_master_node()
+	local bufnr = vim.api.nvim_get_current_buf()
+
+	local prev_sibling = node:prev_sibling()
+
+	ts_utils.swap_nodes(node, prev_sibling, bufnr, true)
+end
+
 vim.api.nvim_set_keymap("n", "vx", '<cmd>lua require("myPlugs").select()<cr>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "vu", '<cmd>lua require("myPlugs").move()<cr>', { noremap = true, silent = true })
 
 return M
