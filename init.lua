f5e3b1b1c7aa2b9a1f3cdc1d95f397a20dd57d93
Dev Masta <ziontee113@gmail.com>
2022-04-16 05:36:48 +0700

    cmmit

--- init.lua
+++ init.lua
@@ -33,7 +33,7 @@ require("user.vim-submode-config")
 -- Cursor Flasher
 require("user.beacon-config")
 --Custom Plugins
-require("myPlugs")
+-- require("myPlugs")
 require("myPlugs.global")
 
 -- S:imple Configs
