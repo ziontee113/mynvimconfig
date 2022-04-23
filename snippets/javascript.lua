1151264a9dbb8191ba1dea1bf81a5a7d0738b948
Dev Masta <ziontee113@gmail.com>
2022-04-23 22:32:09 +0700

    for js too

--- snippets/javascript.lua
+++ snippets/javascript.lua
@@ -1,5 +1,4 @@
 local ls = require("luasnip") --{{{
-
 local s = ls.s
 local i = ls.i
 local t = ls.t
@@ -12,37 +11,61 @@ local sn = ls.snippet_node
 local fmt = require("luasnip.extras.fmt").fmt
 local rep = require("luasnip.extras").rep
 
--- --
+local snippets, autosnippets = {}, {} --}}}
 
-local snippets = {}
-local autosnippets = {}
+local group = vim.api.nvim_create_augroup("Javascript Snippets", { clear = true })
+local file_pattern = "*.js"
 
-local autocmd = vim.api.nvim_create_autocmd
-local augroup = vim.api.nvim_create_augroup
-local map = vim.keymap.set
-local opts = { noremap = true, silent = true, buffer = true }
-local group = augroup("Javascript Snippets", { clear = true })
-
-local function cs(trigger, nodes, keymap) --> cs stands for create snippet
+local function cs(trigger, nodes, opts) --{{{
 	local snippet = s(trigger, nodes)
+	local target_table = snippets
 
-	if keymap == "auto" then
-		table.insert(autosnippets, snippet)
-	else
-		table.insert(snippets, snippet)
+	local pattern = file_pattern
+	local keymaps = {}
+
+	if opts ~= nil then
+		-- check for custom pattern
+		if opts.pattern then
+			pattern = opts.pattern
+		end
+
+		-- if opts is a string
+		if type(opts) == "string" then
+			if opts == "auto" then
+				target_table = autosnippets
+			else
+				table.insert(keymaps, { "i", opts })
+			end
+		end
+
+		-- if opts is a table
+		if opts ~= nil and type(opts) == "table" then
+			for _, keymap in ipairs(opts) do
+				if type(keymap) == "string" then
+					table.insert(keymaps, { "i", keymap })
+				else
+					table.insert(keymaps, keymap)
+				end
+			end
+		end
+
+		-- set autocmd for each keymap
+		if opts ~= "auto" then
+			for _, keymap in ipairs(keymaps) do
+				vim.api.nvim_create_autocmd("BufEnter", {
+					pattern = pattern,
+					group = group,
+					callback = function()
+						vim.keymap.set(keymap[1], keymap[2], function()
+							ls.snip_expand(snippet)
+						end, { noremap = true, silent = true, buffer = true })
+					end,
+				})
+			end
+		end
 	end
 
-	if keymap ~= nil then
-		autocmd("BufEnter", {
-			pattern = "*.js",
-			group = group,
-			callback = function()
-				map({ "i" }, keymap, function()
-					ls.snip_expand(snippet)
-				end, opts)
-			end,
-		})
-	end
+	table.insert(target_table, snippet) -- insert snippet into appropriate table
 end --}}}
 
 -- Old Style --
@@ -155,7 +178,7 @@ while ({}) {{
 		}
 	)
 ) --}}}
-cs({ trig = "cl" }, { t("console.log("), i(1, ""), t(")") }, "jcl") -- console.log
+cs("cl", { t("console.log("), i(1, ""), t(")") }, { "jcl", "jj" }) -- console.log
 
 -- End Refactoring --
 
