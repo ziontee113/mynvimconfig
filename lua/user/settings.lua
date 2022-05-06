526da391b28d38accc732194df263f3afa6b0b39
Dev Masta <ziontee113@gmail.com>
2022-05-07 04:54:04 +0700

    sml changes

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -76,7 +76,6 @@ vim.cmd([[set runtimepath+=~/.config/nvim/lua/myPlugs/supporting-acctress/]])
 
 -- LSP Help Background
 vim.cmd([[:hi NormalFloat guibg=NONE]])
-vim.cmd([[:hi WildMenu guibg=NONE]])
 -- vim.cmd([[:hi FloatBorder guibg=NONE]])
 
 vim.cmd(":hi MsgArea guifg=#008cff")
