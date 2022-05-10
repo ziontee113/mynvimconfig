b626df4496908b9c3bc53831a29b5f3e34c55f37
Dev Masta <ziontee113@gmail.com>
2022-05-11 03:32:29 +0700

    knowing a little bit of caching with setmetatable, still don't fully understand how to use metatables

--- test.lua
+++ test.lua
@@ -14,10 +14,34 @@
 --
 -- theFeels(200, "we don't talk about Bruno")
 
-local table = {}
-table["/2.3/search/advanced?order=desc&sort=relevance&q=how%20to%20center%20a%20div%20in%20html&site=stackoverflow"] =
-	"dalla dalla"
+-- local table = {}
+-- table["/2.3/search/advanced?order=desc&sort=relevance&q=how%20to%20center%20a%20div%20in%20html&site=stackoverflow"] =
+-- 	"dalla dalla"
+--
+-- print(
+-- 	table["/2.3/search/advanced?order=desc&sort=relevance&q=how%20to%20center%20a%20div%20in%20html&site=stackoverflow"]
+-- )
 
-print(
-	table["/2.3/search/advanced?order=desc&sort=relevance&q=how%20to%20center%20a%20div%20in%20html&site=stackoverflow"]
-)
+-----------------------------------------------------
+
+local my_expensive_function = function(x)
+	return x * x
+end
+
+local meta = setmetatable({ cache = {} }, {
+	__call = function(self, val)
+		local ret = self[val]
+		if ret ~= nil then
+			print("cached")
+			return ret
+		else
+			ret = my_expensive_function(val)
+			print("not cached")
+			self[val] = ret
+			return ret
+		end
+	end,
+})
+
+print(meta(3))
+print(meta(3))
