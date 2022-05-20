47d312c7f99e3db120b5a2eb74e8e7337a18dc03
Dev Masta <ziontee113@gmail.com>
2022-05-20 08:56:15 +0700

    one step closer

--- lua/testing/tree_testing.lua
+++ lua/testing/tree_testing.lua
@@ -29,36 +29,62 @@ local function get_nodes_in_array()
 	return nodes
 end
 
+-- helper function has_value
+local function has_value(tab, val)
+	for index, value in ipairs(tab) do
+		if value == val then
+			return true
+		end
+	end
+
+	return false
+end
+
 -- loop through nodes and print their type
-local function print_types()
+local dictionary = {
+	["function"] = {
+		color_group = "DapUIScope",
+		alias = "F",
+	},
+	["variable_declaration"] = {
+		color_group = "DapUIScope",
+		alias = "v",
+	},
+	["if_statement"] = {
+		color_group = "DapUIScope",
+		alias = "I",
+	},
+	["for_statement"] = {
+		color_group = "DapUIScope",
+		alias = "F",
+	},
+}
+local function print_types(desired_types)
 	local nodes = get_nodes_in_array()
-	local desired_types = { "function", "variable_declaration", "if_statement", "for_statement" }
-	local desired_aliases = { "F", "v", "I", "F" }
 
 	vim.cmd([[:messages clear]])
 	for _, node in ipairs(nodes) do
-		print(node:type())
+		local node_type = node:type()
+		print(node_type)
 
-		-- loop through desired_types
-		for index, desired_type in ipairs(desired_types) do
-			if node:type() == desired_type then
-				---@diagnostic disable-next-line: unused-local
-				local start_row, start_col, end_row, end_col = node:range()
+		-- check if the node type is in the desired_types
+		if has_value(desired_types, node_type) then
+			local color_group = dictionary[node_type].color_group
+			local alias = dictionary[node_type].alias
+			---@diagnostic disable-next-line: unused-local
+			local start_row, start_col, end_row, end_col = node:range()
 
-				local alias = desired_aliases[index]
-
-				api.nvim_buf_set_extmark(0, ns, start_row, start_col, {
-					virt_text = { { alias, "DapUIScope" } },
-					virt_text_pos = "overlay",
-				})
-			end
+			api.nvim_buf_set_extmark(0, ns, start_row, start_col, {
+				virt_text = { { alias, color_group } },
+				virt_text_pos = "overlay",
+			})
 		end
 	end
 end
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set("n", " pt", function()
-	print_types()
+	print_types({ "variable_declaration" })
 end, opts)
 
 vim.keymap.set("n", " me", ":messages<cr>", opts)
